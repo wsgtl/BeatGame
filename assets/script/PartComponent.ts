d75bbad079ed99fbc768f42077a01cec9118cb72
2568271372@qq.com <2568271372@qq.com>
2024-01-05 11:05:11 +0800

    feat:init

---
+++ assets/script/PartComponent.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('PartComponent')
+export class PartComponent extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
